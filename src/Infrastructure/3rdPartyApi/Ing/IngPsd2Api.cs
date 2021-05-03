ad02dbaf1ae4f298bd78a05216d8480608d08abf
limon <igorz.jigan@gmail.com>
2021-05-03 14:39:37 +0300

    Added sandbox certificates.

---
+++ src/Infrastructure/3rdPartyApi/Ing/IngPsd2Api.cs
@@ -0,0 +1,11 @@
+﻿using Ing.Interview.Application.Common.Interfaces;
+using Ing.Interview.Domain.Entities;
+
+namespace Ing.Interview.Infrastructure._3rdPartyApi.Ing
+{
+    internal class IngPsd2Api : IApplicationDbContext
+    {
+        public IStorage<Account> Accounts { get; }
+        public IStorage<Transaction> Transactions { get; }
+    }
+}
